2b0e2da7abf3578ecddf6de11f73837dabf887e7
Rana Vikrant Singh Rathod <ranavikrantsinghrathod@Ranas-MacBook-Pro.local>
2022-10-11 17:28:22 +0530

    changes done

--- src/screens/login/OtpScreen.js
+++ src/screens/login/OtpScreen.js
@@ -5,10 +5,17 @@ import DynamicButton from '../../components/DynamicButton';
 import {scale} from '../../utils/scaling';
 const OtpScreen = props => {
   const {contact} = props?.route?.params;
+  var replaced = contact.replace(/^(.{2}).*(.{3}).*(.{4})$/, `$1****$3`);
+
+
   const handleOnLogin = () => {};
   return (
     <SafeAreaView style={styles.mainContainer}>
       <View style={styles.insideContainer}>
+        <Text style={styles.welcomeText}>Welcome</Text>
+        <Text style={styles.belowText}>
+          Please enter the otp sent to {replaced}
+        </Text>
         <OTPInputView
           style={{width: '80%', height: 200}}
           pinCount={4}
@@ -19,7 +26,7 @@ const OtpScreen = props => {
             console.log(`Code is ${code}, you are good to go!`);
           }}
         />
-        <DynamicButton>Login</DynamicButton>
+        <DynamicButton onPress={() => handleOnLogin()}>Login</DynamicButton>
       </View>
     </SafeAreaView>
   );
@@ -34,5 +41,17 @@ const styles = StyleSheet.create({
   },
   insideContainer: {
     marginHorizontal: scale(23),
+  },
+  welcomeText: {
+    fontSize: scale(30),
+    paddingVertical: scale(20),
+  },
+  belowText: {
+    fontSize: scale(16),
+    paddingBottom: scale(13),
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
