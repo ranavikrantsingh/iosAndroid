956f7f0d664d80773ddc63d2173a88e8204ffeaa
Rana Vikrant Singh Rathod <ranavikrantsinghrathod@Ranas-MacBook-Pro.local>
2022-10-12 09:28:44 +0530

    redux setup

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -2,13 +2,21 @@ import {StyleSheet, Text, View, SafeAreaView, StatusBar} from 'react-native';
 import React from 'react';
 import all_styles from '../../styles/all_styles';
 import {scale} from '../../utils/scaling';
+import DynamicButton from '../../components/DynamicButton';
+import { useDispatch } from 'react-redux';
+import {logout} from '../../redux/actions';
+
 const HomeScreen = props => {
+  const dispatch=useDispatch()
   return (
     <SafeAreaView style={styles.mainContainer}>
       <StatusBar backgroundColor={'#fff'} barStyle={'dark-content'} />
       <View style={styles.insideContainer}>
         <Text style={all_styles.span_18_b}>HomeScreen</Text>
       </View>
+      <DynamicButton onPress={()=>dispatch(logout())}>
+        Logout
+      </DynamicButton>
     </SafeAreaView>
   );
 };
