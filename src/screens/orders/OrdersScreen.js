79561773edbc268ad46a9e68533fea5d36bd5f99
Rana Vikrant Singh Rathod <ranavikrantsinghrathod@Ranas-MacBook-Pro.local>
2022-10-15 11:59:55 +0530

    changes done

--- src/screens/orders/OrdersScreen.js
+++ src/screens/orders/OrdersScreen.js
@@ -1,14 +1,60 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import {StyleSheet, Text, SafeAreaView,StatusBar} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {useSelector} from 'react-redux';
+import { scale } from '../../utils/scaling';
+import { useIsFocused } from '@react-navigation/native';
+import Colors from '../../constants/Colors';
+import all_styles from '../../styles/all_styles';
 const OrdersScreen = () => {
+  const [user, setUser] = useState(
+    useSelector(state => state?.appReducer?.user),
+  );
+  const theme = useSelector(state => state.appReducer);
+  const [mode, setMode] = useState(theme.mode);
+
+  function FocusAwareStatusBar(props) {
+    const isFocused = useIsFocused();
+
+    return isFocused ? <StatusBar {...props} /> : null;
+  }
+  useEffect(() => {
+    setMode(theme.mode);
+  }, [theme]);
   return (
-    <View>
-      <Text>OrdersScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView
+      style={mode == 'dark' ? styles.darkModeContainer : styles.mainContainer}>
+      <FocusAwareStatusBar
+        backgroundColor={mode == 'dark' ? '#121212' : '#fff'}
+        barStyle={mode == 'dark' ? 'light-content' : 'dark-content'}
+      />
+      </SafeAreaView>
+  );
+};
 
-export default OrdersScreen
+export default OrdersScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  darkModeContainer: {
+    flex: 1,
+    backgroundColor: Colors.darkMode,
+  },
+  insideContainer: {
+    marginHorizontal: scale(23),
+  },
+  welcomeText: {
+    fontSize: scale(20),
+    fontFamily: 'honc-Bold',
+    color: '#000',
+    paddingVertical: scale(20),
+  },
+  darkmodeWelcomeText: {
+    fontSize: scale(20),
+    fontFamily: 'honc-Bold',
+    color: '#fff',
+    paddingVertical: scale(20),
+  },
+});
