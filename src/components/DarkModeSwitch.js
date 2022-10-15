9f9a66a4e567f928b8f88c7580e67e903780d728
Rana Vikrant Singh Rathod <ranavikrantsinghrathod@Ranas-MacBook-Pro.local>
2022-10-15 15:59:42 +0530

    custom DarkMode switch

---
+++ src/components/DarkModeSwitch.js
@@ -0,0 +1,35 @@
+import {StyleSheet, Animated, View, TouchableOpacity} from 'react-native';
+import React, {useState, useRef} from 'react';
+import LottieView from 'lottie-react-native';
+
+const CheckBoxSquare = props => {
+  const progress = useRef(new Animated.Value(0)).current;
+  const [animate, setAnimate] = useState(false);
+  const handleAnimation = () => {
+    const animatedValue = animate ? 0 : 1;
+    Animated.timing(progress, {
+      toValue: animatedValue,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+    setAnimate(!animate);
+  };
+  return (
+    <TouchableOpacity
+      onPressIn={() => {
+        handleAnimation()
+      }} onPress={props.onPress}>
+      <View>
+        <LottieView
+          source={require('../assets/animations/DarkMode.json')}
+          progress={progress}
+          autoSize
+        />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default CheckBoxSquare;
+
+const styles = StyleSheet.create({});
