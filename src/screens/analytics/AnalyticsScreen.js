eb8e4706de7277a25cf242ceb1eeaadcda1bcf6d
Rana Vikrant Singh Rathod <ranavikrantsinghrathod@Ranas-MacBook-Pro.local>
2022-10-23 18:39:54 +0530

    changes done

--- src/screens/analytics/AnalyticsScreen.js
+++ src/screens/analytics/AnalyticsScreen.js
@@ -1,4 +1,11 @@
-import {View, Text, StatusBar, StyleSheet, FlatList} from 'react-native';
+import {
+  View,
+  Text,
+  StatusBar,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
 import React, {useState} from 'react';
 import Chat from './components/Chat';
 import LinearGradient from 'react-native-linear-gradient';
@@ -100,7 +107,11 @@ export default function DeleteChat() {
       <View style={styles.container}>
         <View style={styles.chatHeader}>
           <Text style={styles.chatTxt}>Chat</Text>
-          <AntIcon name="plus" size={25} color="#4283d7" />
+          <TouchableOpacity>
+            <View>
+              <AntIcon name="plus" size={25} color="#4283d7" />
+            </View>
+          </TouchableOpacity>
         </View>
 
         <FlatList
@@ -144,73 +155,3 @@ const styles = StyleSheet.create({
   },
   chatTxt: {fontSize: 25, fontWeight: 'bold'},
 });
-
-
-
-// import {StyleSheet, Text, SafeAreaView,StatusBar} from 'react-native';
-// import React, {useState, useEffect} from 'react';
-// import {useSelector} from 'react-redux';
-// import { scale } from '../../utils/scaling';
-// import { useIsFocused } from '@react-navigation/native';
-// import Colors from '../../constants/Colors';
-// import { CloseHeader } from '../../components/Header';
-// import all_styles from '../../styles/all_styles';
-// const AnalyticsScreen = (props) => {
-//   const [user, setUser] = useState(
-//     useSelector(state => state?.appReducer?.user),
-//   );
-//   const theme = useSelector(state => state.appReducer);
-//   const [mode, setMode] = useState(theme.mode);
-
-//   function FocusAwareStatusBar(props) {
-//     const isFocused = useIsFocused();
-
-//     return isFocused ? <StatusBar {...props} /> : null;
-//   }
-//   useEffect(() => {
-//     setMode(theme.mode);
-//   }, [theme]);
-//   return (
-//     <SafeAreaView
-//       style={mode == 'dark' ? styles.darkModeContainer : styles.mainContainer}>
-//         <CloseHeader
-//       text="Analytics"
-//       onPress={()=>props.navigation.navigate('DrawerNavigator')}
-
-//       fill={mode == 'dark' ? '#121212' : '#fff'}
-//       />
-//       <FocusAwareStatusBar
-//         backgroundColor={mode == 'dark' ? '#121212' : '#fff'}
-//         barStyle={mode == 'dark' ? 'light-content' : 'dark-content'}
-//       />
-//       </SafeAreaView>
-//   );
-// };
-
-// export default AnalyticsScreen;
-
-// const styles = StyleSheet.create({
-//   mainContainer: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//   },
-//   darkModeContainer: {
-//     flex: 1,
-//     backgroundColor: Colors.darkMode,
-//   },
-//   insideContainer: {
-//     marginHorizontal: scale(23),
-//   },
-//   welcomeText: {
-//     fontSize: scale(20),
-//     fontFamily: 'honc-Bold',
-//     color: '#000',
-//     paddingVertical: scale(20),
-//   },
-//   darkmodeWelcomeText: {
-//     fontSize: scale(20),
-//     fontFamily: 'honc-Bold',
-//     color: '#fff',
-//     paddingVertical: scale(20),
-//   },
-// });
