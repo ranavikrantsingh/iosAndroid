ee936520cf15a43b22dc67c9ddae2383846fabd6
Rana Vikrant Singh Rathod <ranavikrantsinghrathod@Ranas-MacBook-Pro.local>
2022-10-23 19:14:17 +0530

    Gesture Handler Fixes

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './src/redux/store';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {store, persistor} from './src/redux/store';
 import Root from './src/routes/Root';
-import { NativeBaseProvider } from 'native-base';
-
+import {NativeBaseProvider} from 'native-base';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 export default function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <NativeBaseProvider>
-          <Root />
+          <GestureHandlerRootView style={{flex: 1}}>
+            <Root />
+          </GestureHandlerRootView>
         </NativeBaseProvider>
       </PersistGate>
     </Provider>
