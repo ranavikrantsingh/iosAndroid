9cf9cd6f14ac84866d3ed711ead14cafd4c5c817
Rana Vikrant Singh Rathod <ranavikrantsinghrathod@Ranas-MacBook-Pro.local>
2022-10-24 13:01:25 +0530

    changes done

--- src/screens/wallet/WalletScreen.js
+++ src/screens/wallet/WalletScreen.js
@@ -1,49 +1,64 @@
 import {View, Text, StyleSheet, StatusBar, SafeAreaView} from 'react-native';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import UserCard from './components/UserCard';
 import TapToPay from './components/TapToPay';
-
+import {useSelector} from 'react-redux';
+import {useIsFocused} from '@react-navigation/native';
+import Colors from '../../constants/Colors'
 const BkashPayment = () => {
+  const theme = useSelector(state => state.appReducer);
+  const [mode, setMode] = useState(theme.mode);
+
+  function FocusAwareStatusBar(props) {
+    const isFocused = useIsFocused();
+
+    return isFocused ? <StatusBar {...props} /> : null;
+  }
+  useEffect(() => {
+    setMode(theme.mode);
+  }, [theme]);
   return (
-    <>
-      <StatusBar backgroundColor="#fff" />
-      <SafeAreaView style={style.container}>
-        <View style={style.innerContent}>
-          <View style={{flex: 1, paddingHorizontal: 20}}>
-            <Text style={style.heading}>
-              Confirm to <Text style={style.bold}>Mobile Recharge</Text>
-            </Text>
-            <UserCard
-              title="Rana Vikrant Singh Rathod"
-              subTitle="9666836140"
-              avatarText="R"
-              avatarColor="#e2136e"
-            />
-            <View style={style.boxArea}>
-              <View style={style.Row}>
-                <View style={style.col}>
-                  <Text>Total</Text>
-                  <Text>$10.00</Text>
-                </View>
-                <View style={style.col}>
-                  <Text>New Balance</Text>
-                  <Text>$2710.00</Text>
-                </View>
-                <View style={style.col}>
-                  <Text>Type</Text>
-                  <Text>Prepaid</Text>
-                </View>
-                <View style={style.col}>
-                  <Text>Mobile Operator</Text>
-                  <Text>Airtel</Text>
-                </View>
+    <SafeAreaView style={style.container}>
+      <FocusAwareStatusBar
+        backgroundColor={mode == 'dark' ? 'rgba(0,0,0,0.4)' : '#fff'}
+        barStyle={mode == 'dark' ? 'light-content' : 'dark-content'}
+      />
+
+      <View style={mode == 'dark' ? style.darkContent :style.innerContent}>
+        <View style={{flex: 1, paddingHorizontal: 20}}>
+          <Text style={style.heading}>
+            Confirm to <Text style={style.bold}>Mobile Recharge</Text>
+          </Text>
+          <UserCard
+            title="Rana Vikrant Singh Rathod"
+            subTitle="9666836140"
+            avatarText="R"
+            avatarColor="#e2136e"
+          />
+          <View style={style.boxArea}>
+            <View style={style.Row}>
+              <View style={style.col}>
+                <Text>Total</Text>
+                <Text>$10.00</Text>
+              </View>
+              <View style={style.col}>
+                <Text>New Balance</Text>
+                <Text>$2710.00</Text>
+              </View>
+              <View style={style.col}>
+                <Text>Type</Text>
+                <Text>Prepaid</Text>
+              </View>
+              <View style={style.col}>
+                <Text>Mobile Operator</Text>
+                <Text>Airtel</Text>
               </View>
             </View>
           </View>
-          <TapToPay />
         </View>
-      </SafeAreaView>
-    </>
+        <TapToPay />
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -53,10 +68,14 @@ const style = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'rgba(0,0,0,0.4)',
-    // padding: 20,
+    padding: 20,
   },
   innerContent: {
     backgroundColor: 'white',
+    flex: 1,
+  },
+  darkContent: {
+    backgroundColor: Colors.darkMode,
     flex: 1,
   },
   heading: {
--- src/screens/wallet/components/UserCard.js
+++ src/screens/wallet/components/UserCard.js
@@ -1,14 +1,30 @@
 import {View, Text, StyleSheet} from 'react-native';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import {useSelector} from 'react-redux';
+import Colors from '../../../constants/Colors';
 
 const UserCard = ({title, subTitle, description, avatarText, avatarColor}) => {
+  const theme = useSelector(state => state.appReducer);
+  const [mode, setMode] = useState(theme.mode);
+
+  useEffect(() => {
+    setMode(theme.mode);
+  }, [theme]);
   return (
-    <View style={style.row}>
+    <View style={mode == 'dark' ? style.darkRow : style.row}>
       <Icon name="user-circle" color="grey" size={45} />
       <View style={style.mailText}>
-        <Text style={[style.title, style.bold]}>{title}</Text>
-        <Text style={[style.subTitle, style.bold]} numberOfLines={1}>
+        <Text
+          style={[mode == 'dark' ? style.darkTitle : style.title, style.bold]}>
+          {title}
+        </Text>
+        <Text
+          style={[
+            mode == 'dark' ? style.darkSubTitle : style.subTitle,
+            style.bold,
+          ]}
+          numberOfLines={1}>
           {subTitle}
         </Text>
       </View>
@@ -26,6 +42,13 @@ const style = StyleSheet.create({
     paddingHorizontal: 10,
     backgroundColor: 'white',
   },
+  darkRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    backgroundColor: Colors.darkMode,
+  },
   mailText: {
     marginLeft: 15,
     width: '80%',
@@ -33,10 +56,20 @@ const style = StyleSheet.create({
   bold: {
     fontWeight: 'bold',
   },
+  darkTitle: {
+    fontSize: 16,
+    color: '#fff',
+  },
   title: {
     fontSize: 16,
+    color: '#000',
   },
   subTitle: {
     fontSize: 13,
+    color: '#000',
+  },
+  darkSubTitle: {
+    fontSize: 13,
+    color: '#fff',
   },
 });
